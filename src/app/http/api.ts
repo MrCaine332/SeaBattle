6016b951645fb749ea5d9ed778901bc3678457e4
Egor <ktp332@yandex.ru>
2023-02-17 21:35:46 +0300

    Changes

--- src/app/http/api.ts
+++ src/app/http/api.ts
@@ -1,8 +1,8 @@
 import axios, {InternalAxiosRequestConfig} from 'axios';
 
-export const API_URL = `https://game.hobbs-it.ru/api`
+// export const API_URL = `https://game.hobbs-it.ru/api`
 // export const API_URL = `http://localhost:5035/api`
-// export const API_URL = `http://80.249.145.107/api`
+export const API_URL = `http://80.249.145.107/api`
 
 
 const $api = axios.create({
